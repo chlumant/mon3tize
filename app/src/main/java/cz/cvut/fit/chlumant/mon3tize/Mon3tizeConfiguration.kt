4e3d4e68d38be8c3aa8a4a34370bb70e525073ad
Antonín Chlum <chlumant@fit.cvut.cz>
2025-04-01 11:42:27 +0200

    implementing thesis consultation feedback

--- app/src/main/java/cz/cvut/fit/chlumant/mon3tize/Mon3tizeConfiguration.kt
+++ app/src/main/java/cz/cvut/fit/chlumant/mon3tize/Mon3tizeConfiguration.kt
@@ -1,14 +1,6 @@
 package cz.cvut.fit.chlumant.mon3tize
 
-sealed interface AdMob {
-    data object Disabled : AdMob
-
-    data class Enabled(
-        val key: String
-    ) : AdMob
-}
-
 data class Mon3tizeConfiguration(
-    val enableFreemium: Boolean = true,
-    val adMob: AdMob = AdMob.Disabled
+    val enableFreemium: Boolean = false,
+    val adMobEnabled: Boolean = false
 )

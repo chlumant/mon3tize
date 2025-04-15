2ba26bef1780281940262d5a4527d782cde9a5e5
Antonín Chlum <chlumant@fit.cvut.cz>
2025-04-15 16:25:33 +0200

    adding freemium view model

---
+++ app/src/main/java/cz/cvut/fit/chlumant/demoApp/viewmodels/FreemiumViewModel.kt
@@ -0,0 +1,35 @@
+package cz.cvut.fit.chlumant.demoApp.viewmodels
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import cz.cvut.fit.chlumant.mon3tize.Mon3tize
+import kotlinx.coroutines.flow.SharingStarted
+import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.launch
+
+class FreemiumViewModel : ViewModel() {
+
+    val isFreemiumActive = Mon3tize.isFreemiumActive
+        .stateIn(viewModelScope, SharingStarted.Lazily, false)
+
+    val isFirstLaunch = Mon3tize.isFirstLaunch
+        .stateIn(viewModelScope, SharingStarted.Lazily, true)
+
+    fun startTrial() {
+        viewModelScope.launch {
+            Mon3tize.freemiumManager.enableFreemium()
+        }
+    }
+
+    fun disableFreemium() {
+        viewModelScope.launch {
+            Mon3tize.freemiumManager.disableFreemium()
+        }
+    }
+
+    fun setFirstLaunch(value: Boolean) {
+        viewModelScope.launch {
+            Mon3tize.setFirstLaunch(value)
+        }
+    }
+}
